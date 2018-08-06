9650323e6c698ff058394d4c4ba6c964c645d320
Bryan Bacolod <bacolodbryan8@gmail.com>
2018-08-06 17:07:43 +0800

    Clear challenge

---
+++ app/src/main/java/com/example/pc002/challenge_3_lab_exercise_1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.pc002.challenge_3_lab_exercise_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void Clear (View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        editText.setText("");
+    }
+
+}
